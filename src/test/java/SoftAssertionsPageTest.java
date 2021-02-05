b56cf4dd82e580c0c9eb26f220a497a6fb7f4496
akiselef <rage4ndlove@gmail.com>
2021-02-05 13:57:10 +0400

    default task add

--- src/test/java/SoftAssertionsPageTest.java
+++ src/test/java/SoftAssertionsPageTest.java
@@ -1,7 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SoftAssertionsPageTest {
     @Test
     void softAssertionsTest() {
+        open("https://github.com/selenide/selenide");
+        $("[data-tab-item=i4wiki-tab]").click();
+        $x("//a[text()='SoftAssertions']").shouldBe(visible);
+        $x("//a[text()='SoftAssertions']").click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
 }
